5f3bd3fef18770b20d3e2fda1e3018a881468a2e
inemieboka96 <inemieboka96@gmail.com>
2026-02-24 11:06:13 +0100

    Refactor app.js to implement basic routing and logger middleware

--- app.js
+++ app.js
@@ -10,8 +10,31 @@ app.get("/", (req, res) => {
   res.send("Hello World!!!");
 });
 
-// Starting Server
+/*
+    ROUTING
+*/
 
+// GET Routes
+app.get("/about", (req, res) => {
+  res.send("About Page📃");
+});
+
+// POST Routes
+app.post("/submit", (req, res) => {
+  res.send("Form Submitted✅");
+});
+
+/*
+    MIDDLEWARE
+*/
+
+// Simple Logger Middleware
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString}] ${req.method} ${req.url}`);
+  next(); // Move to the next middleware/route
+});
+
+// Starting Server
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
 });
